83f67d4f0ed1af257e514cb259368763e89dcbb9
elenajim6 <maryjimenezbts@gmail.com>
2022-06-09 09:59:18 -0500

    Se agregaron los demas campos para el usuario en config

--- crud/api/2022-2/src/config.php
+++ crud/api/2022-2/src/config.php
@@ -7,8 +7,8 @@ $user = 'root';
 $pass = 'r00t_p4ssw0rd';
 $db   = 'db';
 
-$arr_campos_usuarios = array('username');
+$arr_campos_usuarios = array('username','nombre', 'ap_paterno', 'ap_materno');
 
-$arr_campos_usuarios_nn = array('username');
+$arr_campos_usuarios_nn = array('username','nombre', 'ap_paterno', 'ap_materno');
 
 ?>
