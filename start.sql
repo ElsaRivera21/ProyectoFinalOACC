0f8f0924a4569e4f9baa5f1a8cca929bbc1ef69e
ibzan79 <ibzan8davila@gmail.com>
2022-06-09 09:20:29 -0500

    Cambios en la tabla usuarios

--- start.sql
+++ start.sql
@@ -2,7 +2,10 @@ use db;
 
 CREATE TABLE IF NOT EXISTS usuarios
 (    id     INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `username` VARCHAR(255) NOT NULL
+    `username` VARCHAR(255) NOT NULL,
+    `nombre` VARCHAR(60) NOT NULL,
+    `ap_paterno` VARCHAR(60) NOT NULL,
+    `ap_materno` VARCHAR(60) NOT NULL
 );
 
 INSERT INTO usuarios (`username`) VALUES ('Aaron');
